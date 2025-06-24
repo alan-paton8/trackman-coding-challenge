5d8ee750f0a1e933ddcd8d8a56bddac49cab556d
Alan Paton <abp@dwarf.dk>
2025-06-25 01:17:40 +0200

    chore: Commit missing files

--- app/assets/icons/icons.ts
+++ app/assets/icons/icons.ts
@@ -1,3 +1,4 @@
 export { default as Location } from "./location.svg";
 export { default as Delete } from "./delete.svg";
 export { default as Logo } from "./logo.svg";
+export { default as Star } from "./star.svg";
